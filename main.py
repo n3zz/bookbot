87ede3d9916521d5ea8db7b24c68ddcb034cacfa
n3zz <n3xi@protonmail.com>
2025-03-08 02:15:24 +0000

    Formatted, filtered and sorted the alphabet in terms of numerical count and then formatted the display

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from stats import get_book_words
 from stats import dictionary_maker
+from stats import process_list
+from stats import sort_on
+from stats import filtering_list
+from stats import display_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,7 +13,12 @@ def get_book_text(filepath):
 def main():
     word_count = get_book_words("books/frankenstein.txt")
     book_text = get_book_text("books/frankenstein.txt")
-    print(f"{word_count} words found in the document")
-    print(dictionary_maker(book_text))
+    print(f"Found {word_count} total words")
+    # print(dictionary_maker(book_text))
+    book_dict = dictionary_maker(book_text)
+    processed_list = process_list(book_dict)
+    sorted_list = sort_on(processed_list)
+    
+    display_list(sorted_list)
 
 main()
--- stats.py
+++ stats.py
@@ -8,7 +8,6 @@ def get_book_words(filepath):
 
 def dictionary_maker(booktext):
     char_dict = {}
-    
     for char in booktext:
         lowered_char = char.lower()
         if lowered_char in char_dict:
@@ -16,3 +15,26 @@ def dictionary_maker(booktext):
         else:
             char_dict[lowered_char] = 1
     return char_dict
+
+def process_list(raw_dict):
+    char_list = []
+    for char, count in raw_dict.items():
+        char_list.append({"char": char, "count": count})
+    return char_list
+
+def sort_on(list):
+    new_list = sorted(list, key=lambda item: item["count"], reverse=True)
+    return new_list
+
+def filtering_list(sorted_list):
+    filtered_list = []
+    for list in sorted_list:
+        if list["char"].isalpha() == True:
+            filtered_list.append(list)
+        else:
+            pass
+    return filtered_list
+
+def display_list(final_list):
+    for item in final_list:
+        print(f"{item["char"]}: {item["count"]}")
