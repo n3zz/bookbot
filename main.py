0c7321b9ce1ad7fae1027732f1a73e5fe1311de8
n3zz <n3xi@protonmail.com>
2025-03-08 01:26:57 +0000

    Added counted words and characters

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from stats import get_book_words
+from stats import dictionary_maker
 
 def get_book_text(filepath):
     with open(filepath) as f:
-        book_text = f.read()
-    return book_text
-
+        return f.read()
+        
 
 def main():
-    print(get_book_words("books/frankenstein.txt"))
-
+    word_count = get_book_words("books/frankenstein.txt")
+    book_text = get_book_text("books/frankenstein.txt")
+    print(f"{word_count} words found in the document")
+    print(dictionary_maker(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,18 @@
 def get_book_words(filepath):
     with open(filepath) as f:
         book_text = f.read()
-    
     text_list = book_text.split()
     no_of_words = len(text_list)
 
-    return f"{no_of_words} words found in the document"
+    return no_of_words
+
+def dictionary_maker(booktext):
+    char_dict = {}
+    
+    for char in booktext:
+        lowered_char = char.lower()
+        if lowered_char in char_dict:
+            char_dict[lowered_char] += 1
+        else:
+            char_dict[lowered_char] = 1
+    return char_dict
